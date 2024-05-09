96dacd151e55f3d89d9f51e99ee1e8639a57e4a3
AnonymousRaihan <169072506+AnonymousRaihan@users.noreply.github.com>
2024-05-09 11:00:04 +0600

    Anonymous

---
+++ requirement.sh
@@ -0,0 +1,15 @@
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+figlet -f big Done !!! | lolcat
+echo
+echo -e "[1m Now Run bash t-ban.sh...!!!"
+echo
+echo -e "[1m[32m Ꭰ𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝙳 Ᏼ𝚈 :[33m Ꮧ𝙽𝙾𝙽𝚈𝙼𝙾𝚄Ꭶ (PaPi 16)"
---
+++ t-ban.sh
@@ -0,0 +1,31 @@
+clear
+figlet -f big Termux Banner | lolcat
+cyan='[0;36m'
+lightgreen='[1;32m'
+red='[1;31m'
+yellow='[1;33m'
+echo -e $lightgreen "[1m                Ꮪ𝙴𝙲𝚄𝚁𝙸𝚃𝚈 Ꮋ𝙴𝙻𝙿 Ꮀ𝙾𝚁 Ꭼ𝚃𝙷𝙸𝙲𝙰𝙻 Ꮋ𝙰𝙲𝙺𝙴𝚁𝚂... "
+echo " "
+echo -e $yellow "[1m                         -Ꮧ𝙽𝙾𝙽𝚈𝙼𝙾𝚄Ꭶ "
+echo " "
+echo " "
+echo " "
+echo -e "[1m[33m
+Ꮃ𝙷𝙰𝚃 𝙸𝚂 𝚈𝙾𝚄𝚁 [31mᏴ𝙰𝙽𝙽𝙴𝚁 [33mᏁ𝙰𝙼𝙴[32m :
+
+"
+read varbanner
+echo
+echo -e "[1m[33m
+Ꮃ𝙷𝙰𝚃 𝙸𝚂 Ꭹ𝙾𝚄𝚁 Ᏼ𝙸𝙾 Ꮑ𝙰𝙼𝙴[32m :
+
+ "
+read varcowsay
+echo  "cowsay -f eyes "$varcowsay" | lolcat" > C0w54y.txt
+echo "toilet -f big ' $varbanner' -F gay | lolcat" > 84nn3r.txt
+echo
+echo "clear" > cl34r.txt
+cat "cl34r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "C0w54y.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+ls
+cat "84nn3r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
